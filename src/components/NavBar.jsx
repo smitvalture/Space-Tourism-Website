395678548ce819579072c2ea28d5c00040f2e29d
smit_valture <valture16@gmail.com>
2023-05-31 23:34:54 +0530

    nav bar complete responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import star from '../assets/home/star.jpg'
+import ham from '../assets/shared/icon-hamburger.svg'
+import close from '../assets/shared/icon-close.svg'
 
 const NavBar = () => {
+
+    const [menu, setMenu] = useState(false)
+
     return (
-        <nav className='navLink flex justify-between items-center w-full lg:pl-14 pl-6 lg:pt-10 relative'>
-
+        <>
             {/* desktop & tablet Navigation */}
-            <Link to={"/"} className='w-12 h-12 rounded-full overflow-hidden duration-500 hover:scale-90 hover:rotate-180'>
-                <img className='' src={star} alt="star img" />
-            </Link>
+            <nav className='hidden md:flex navLink justify-between items-center w-full lg:pl-14 pl-6 lg:mt-10 relative'>
 
-            <div className='absolute hidden lg:block left-40 w-[36%] z-10 h-[1px] bg-white/20'></div>
+                <Link to={"/"} className='w-12 h-12 rounded-full overflow-hidden duration-500 hover:scale-90 hover:rotate-180'>
+                    <img className='' src={star} alt="star img" />
+                </Link>
 
-            <div className='h-24 lg:w-7/12 lg:pl-32 md:px-12 flex gap-12 justify-start items-center backdrop-blur-lg border-[1px] border-solid border-white border-opacity-[0.04] bg-white/5 text-white/70'>
-                <NavLink to={"/"} className='h-full flex items-center border-b-2 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-1.5 text-white'>00</span> HOME</NavLink>
-                <NavLink to={"/destination"} className='h-full flex items-center border-b-2 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-1.5 text-white'>01</span> DESTINATION</NavLink>
-                <NavLink to={"/crew"} className='h-full flex items-center border-b-2 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-1.5 text-white'>02</span> CREW</NavLink>
-                <NavLink to={"/technology"} className='h-full flex items-center border-b-2 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-1.5 text-white'>03</span> TECHNOLOGY</NavLink>
-            </div>
+                <div className='absolute hidden lg:block left-40 w-[36%] z-10 h-[1px] bg-white/20'></div>
 
-        </nav>
+                <div className='h-24 lg:w-7/12 lg:pl-32 md:px-12 lg:px-0 flex gap-12 justify-start items-center backdrop-blur-lg border-[1px] border-solid border-white border-opacity-[0.04] bg-white/5 text-white/70'>
+                    <NavLink to={"/"} className='h-full flex items-center border-b-2 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-2 text-white'>00</span> HOME</NavLink>
+                    <NavLink to={"/destination"} className='h-full flex items-center border-b-2 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-2 text-white'>01</span> DESTINATION</NavLink>
+                    <NavLink to={"/crew"} className='h-full flex items-center border-b-2 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-2 text-white'>02</span> CREW</NavLink>
+                    <NavLink to={"/technology"} className='h-full flex items-center border-b-2 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-2 text-white'>03</span> TECHNOLOGY</NavLink>
+                </div>
+
+            </nav>
+
+            { /* mobile Navigation */}
+
+            <nav className='flex md:hidden navLink justify-between items-center w-full px-6 pt-6 relative'>
+
+                <Link to={"/"} className='w-10 h-10 rounded-full overflow-hidden duration-500'>
+                    <img className='' src={star} alt="star img" />
+                </Link>
+
+                <img onClick={() => setMenu(!menu)} className='cursor-pointer z-20' src={menu ? close : ham} alt="menu" />
+
+                <div className={`fixed w-64 ${menu ? "right-0" : "-right-[700px]"} h-screen flex flex-col gap-8 pt-28 pl-8 pr-0.5 overflow-hidden backdrop-blur-lg bg-white/10 top-0 duration-500 text-white/70`}>
+                    <NavLink to={"/"} className='flex h-8 items-center border-r-4 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-2 text-white'>00</span> HOME</NavLink>
+                    <NavLink to={"/destination"} className='flex h-8 items-center border-r-4 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-2 text-white'>01</span> DESTINATION</NavLink>
+                    <NavLink to={"/crew"} className='flex h-8 items-center border-r-4 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-2 text-white'>02</span> CREW</NavLink>
+                    <NavLink to={"/technology"} className='flex h-8 items-center border-r-4 border-transparent focus:border-white hover:border-white/25 text-base tracking-[2.7px] font-thin font-Barlow'><span className='font-bold mr-2 text-white'>03</span> TECHNOLOGY</NavLink>
+                </div>
+
+
+            </nav>
+
+        </>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,22 +7,31 @@ import NavBar from '../components/NavBar'
 
 const Home = () => {
   return (
-    <section className='flex w-full h-screen flex-col justify-center items-center relative'>
+    <section className='flex w-full min-h-screen h-full flex-col items-center relative'>
 
       <img className='hidden lg:block min-h-full min-w-[1024px] w-full h-auto fixed -z-50 top-0 left-0 contrast-[1.10]' src={deskIMG} alt="home img" />
-      <img className='hidden md:block lg:hidden min-h-full min-w-[768px] w-full h-auto fixed -z-50 top-0 left-0 contrast-[1.10]' src={tabIMG} alt="home img" />
+      <img className='hidden md:block lg:hidden min-h-full min-w-[768px] w-full h-auto fixed -z-50 top-0 left-0 contrast-[1.10] scale-105' src={tabIMG} alt="home img" />
       <img className='block md:hidden min-h-full min-w-[640px] w-full h-auto fixed -z-50 top-0 left-0 contrast-[1.10]' src={mobIMG} alt="home img" />
 
 
       <NavBar />
 
-      <div className='w-full h-full grid grid-cols-2'>
-        <div className='w-full h-full flex justify-center items-center'>sadad</div>
+      <div className='w-full h-[calc(100vh-138px)] py-14 md:py-28 lg:py-0 grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2'>
+
         <div className='w-full h-full flex justify-center items-center'>
-          <Link className='w-64 h-64 mt-32 flex justify-center items-center bg-white rounded-full duration-500 outline outline-0 hover:outline-[100px] outline-white/20'>
+          <div className='w-[450px] h-[382px] flex flex-col text-white text-center lg:text-left'>
+            <p className='font-Barlow font-light text-[28px] text-[#D0D6F9]'>SO, YOU WANT TO TRAVEL TO</p>
+            <p className='text-[150px] font-Bellefair'>SPACE</p>
+            <p className='text-lg font-thin leading-8 text-[#D0D6F9]'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+          </div>
+        </div>
+
+        <div className='w-full h-full flex justify-center items-center'>
+          <Link className='w-64 h-64 lg:mt-36 flex justify-center items-center bg-white rounded-full duration-500 outline outline-0 hover:outline-[100px] outline-white/20'>
             <p className='text-3xl tracking-[2px] font-Bellefair'>EXPLORE</p>
           </Link>
         </div>
+
       </div>
 
     </section>
